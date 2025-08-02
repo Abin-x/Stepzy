96de4d8919a0acc332f1af30cfd94a7acedc67d2
Abin <gitz358@gmail.com>
2025-08-02 20:20:58 +0530

    responsive

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 
 function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [accountOpen, setAccountOpen] = useState(false);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
@@ -13,11 +14,14 @@ function Header() {
     setMenuOpen(false);
   };
 
+  const toggleAccount = () => {
+    setAccountOpen(!accountOpen);
+  };
+
   return (
     <div className="header-container">
       <div className="header-navbar">
-        <div className="header-logo">Stepzy</div>
-
+        {/* Hamburger */}
         <div
           className={`hamburger-icon ${menuOpen ? 'open' : ''}`}
           onClick={toggleMenu}
@@ -25,14 +29,30 @@ function Header() {
           <i className={`fas ${menuOpen ? 'fa-times' : 'fa-bars'} icon`}></i>
         </div>
 
-        <div
-          className={`header-links ${menuOpen ? 'active' : ''}`}
-          onClick={closeMenu}
-        >
+        {/* Logo */}
+        <div className="header-logo">Stepzy</div>
+
+        
+        {/* Nav Links */}
+        <div className={`header-links ${menuOpen ? 'active' : ''}`} onClick={closeMenu}>
           <NavLink to="#home">Home</NavLink>
           <NavLink to="#about">About</NavLink>
           <NavLink to="#contact">Contact</NavLink>
         </div>
+
+        {/* Account Icon */}
+        <div className="dropdown-account" onClick={toggleAccount}>
+          <i className="fas fa-user icon"></i>
+          <div className={`dropdown-menu ${accountOpen ? 'show' : ''}`}>
+            <NavLink to="#login" onClick={() => setAccountOpen(false)}>
+              Login
+            </NavLink>
+            <NavLink to="#profile" onClick={() => setAccountOpen(false)}>
+              Profile
+            </NavLink>
+          </div>
+        </div>
+
       </div>
     </div>
   );
