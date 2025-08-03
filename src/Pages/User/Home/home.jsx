a9a40fa3429d80b7af09128919094762d5abb002
Abin <gitz358@gmail.com>
2025-08-04 00:43:43 +0530

    update

--- src/Pages/User/Home/home.jsx
+++ src/Pages/User/Home/home.jsx
@@ -14,6 +14,12 @@ const Home = () => {
       </div>
     <div className="home-banner-img">
         <img src="/img/nike-trainers-running-shoes.jpg" alt="Home Banner" />
+      </div><div className="home-banner-img">
+        <img src="/img/red_yellow_nike_shoe_in_black_background_4k_8k_hd_nike-3840x2160.jpg" alt="Home Banner" />
+      </div><div className="home-banner-img">
+        <img src="/img/red_yellow_nike_shoe_in_black_background_4k_8k_hd_nike-3840x2160.jpg" alt="Home Banner" />
+      </div><div className="home-banner-img">
+        <img src="/img/red_yellow_nike_shoe_in_black_background_4k_8k_hd_nike-3840x2160.jpg" alt="Home Banner" />
       </div>
     </>
   );
