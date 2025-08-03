d609d151f288e47be83dfc6a9c1458401be1bd1c
Abin <gitz358@gmail.com>
2025-08-03 23:16:48 +0530

    add image

--- src/Pages/User/Home/home.jsx
+++ src/Pages/User/Home/home.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import './home.css'
+import React from 'react';
+import './home.css';
 
 const Home = () => {
   return (
-    <div className='home-container'>home</div>
-  )
-}
+    <>
+    <div className="home-container">
+      <div className="home-banner-img">
+        <img src="/img/red_yellow_nike_shoe_in_black_background_4k_8k_hd_nike-3840x2160.jpg" alt="Home Banner" />
+      </div>
+    </div>
+    <div className="home-banner-img">
+        <img src="/img/red_yellow_nike_shoe_in_black_background_4k_8k_hd_nike-3840x2160.jpg" alt="Home Banner" />
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
