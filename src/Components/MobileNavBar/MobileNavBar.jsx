ca93dff2b65d82d2ae29528e8e9f2ba7533029fb
Abin <gitz358@gmail.com>
2025-08-25 21:08:20 +0530

    change mobileNavBar

--- src/Components/MobileNavBar/MobileNavBar.jsx
+++ src/Components/MobileNavBar/MobileNavBar.jsx
@@ -13,6 +13,10 @@ function MobileNavBar() {
         <i className="fas fa-shop"></i>
         <span>Shop</span>
       </NavLink>
+       <NavLink to="/you" className="nav-item">
+        <i className="fas fa-user"></i>
+        <span>Account</span>
+      </NavLink>
       <NavLink to="/cart" className="nav-item">
         <i className="fas fa-shopping-cart"></i>
         <span>Cart</span>
@@ -21,14 +25,11 @@ function MobileNavBar() {
         <i className="fas fa-heart"></i>
         <span>Wishlist</span>
       </NavLink>
-      <NavLink to="/you" className="nav-item">
-        <i className="fas fa-user"></i>
-        <span>You</span>
-      </NavLink>
-      <NavLink to="/menu" className="nav-item">
+     
+      {/* <NavLink to="/menu" className="nav-item">
         <i className="fas fa-bars"></i>
         <span>Menu</span>
-      </NavLink>
+      </NavLink> */}
     </div>
   );
 }
