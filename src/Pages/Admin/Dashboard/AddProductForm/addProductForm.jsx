37c3bfbdf7bd4508d09dbb97998b36bea2186aff
Abin <gitz358@gmail.com>
2025-08-28 02:55:54 +0530

    add poductlist

--- src/Pages/Admin/Dashboard/AddProductForm/addProductForm.jsx
+++ src/Pages/Admin/Dashboard/AddProductForm/addProductForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import './AddProductForm.css';
+import './addProductForm.css';
 
 const AddProductForm = () => {
   const [formData, setFormData] = useState({
